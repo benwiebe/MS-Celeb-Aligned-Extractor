4ef0a917466a459343f6334bc75c4217a86cdfd0
Ben Wiebe <me@t3kbau5.com>
2019-11-09 14:53:44 -0500

    Update script and readme
    Script changes:
    - Restructure code
    - Added positional arguments to improve usability
    - Ported to Python 3
    - Bugfixes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-A python script to extract MS-Celeb-1m aligned face images tsv file.
+# MS Celeb TSV Extractor
+
+Python script to extract a MS Celeb TSV file.
+
+## Usage:
+```
+tsv_extractor.py [-h] inputTSV outputFolder
+
+Process MS-Celeb TSV file
+
+positional arguments:
+  inputTSV      MS-Celeb TSV to extract
+  outputFolder  Path to exptract images to
+
+optional arguments:
+  -h, --help    show this help message and exit
+```
--- tsv_extractor.py
+++ tsv_extractor.py
@@ -1,39 +1,69 @@
+# Script to extract a MS-Celeb TSV file
+# Forked from: https://github.com/modanesh/MS-Celeb-Aligned-Extractor
+# 
+# Fork Author: Ben Wiebe (https://github.com/benwiebe)
+# Tested on Python 3.7.4
+
+############### IMPORTS ###############
 import os
 import base64
 import struct
+import argparse
 
-fid = open("/media/deepface/983ADA463ADA2154/face/MsCelebV1-Faces-Aligned/MsCelebV1-Faces-Aligned.tsv", "r")
-base_path = '/home/deepface/users/danesh/ms-celeb-extracted/data/'
-if not os.path.exists(base_path):
-  os.mkdir(base_path)
-bbox_file = open(base_path + '/bboxes.txt', 'w')
-while True:
-  line = fid.readline()
-  if line:
-    data_info = line.split('\t')
-    # 0: Freebase MID (unique key for each entity)
-    # 1: ImageSearchRank
-    # 4: FaceID
-    # 5: bbox
-    # 6: img_data
-    filename = data_info[0] + "/" + data_info[1] + "_" + data_info[4] + ".jpg"
-    bbox = struct.unpack('ffff', data_info[5].decode("base64"))
-    bbox_file.write(filename + " "+ (" ".join(str(bbox_value) for bbox_value  in bbox)) + "\n")
+############### GLOBALS ###############
+inputTSV = ''
+outputFolder = ''
 
-    img_data = data_info[6].decode("base64")
-    output_file_path = base_path + "/" + filename 
-    if os.path.exists(output_file_path):
-      print output_file_path + " exists"
+############### SETUP METHOD ###############
+def setup():
+  global inputTSV
+  global outputFolder
 
-    output_path = os.path.dirname(output_file_path)
-    if not os.path.exists(output_path):
-      os.mkdir(output_path)
+  parser = argparse.ArgumentParser(description='Process MS-Celeb TSV file')
+  parser.add_argument('inputTSV', help='MS-Celeb TSV to extract')
+  parser.add_argument('outputFolder', help='Path to exptract images to')
+  args = parser.parse_args()
 
-    img_file = open(output_file_path, 'w')
-    img_file.write(img_data)
-    img_file.close()
-  else:
-    break
+  inputTSV = os.path.normpath(args.inputTSV)
+  outputFolder = os.path.normpath(args.outputFolder)
 
-bbox_file.close()
-fid.close()
+############### MAIN METHOD ###############
+def main():
+  fid = open(inputTSV, "r")
+  if not os.path.exists(outputFolder):
+    os.mkdir(outputFolder)
+
+  with open(inputTSV, "r", encoding='utf-8') as fid:
+    with open(outputFolder + '/bboxes.txt', 'w') as bbox_file:
+      while True:
+        line = fid.readline()
+        if line:
+          data_info = line.split('\t')
+          # 0: Freebase MID (unique key for each entity)
+          # 1: ImageSearchRank
+          # 4: FaceID
+          # 5: bbox
+          # 6: img_data
+          filename = data_info[0] + os.path.sep + data_info[1] + "_" + data_info[4] + ".jpg"
+          bbox = struct.unpack('ffff', base64.b64decode(data_info[5]))
+          bbox_file.write(filename + " "+ (" ".join(str(bbox_value) for bbox_value  in bbox)) + "\n")
+
+          img_data = base64.b64decode(data_info[6])
+          output_file_path = outputFolder + os.path.sep + filename 
+          if os.path.exists(output_file_path):
+            print(output_file_path + " exists")
+
+          output_path = os.path.dirname(output_file_path)
+          if not os.path.exists(output_path):
+            os.mkdir(output_path)
+
+          img_file = open(output_file_path, 'wb')
+          img_file.write(img_data)
+          img_file.close()
+        else:
+          break
+
+############### DEFAULT ENTRYPOINT ###############
+if __name__ == '__main__':
+  setup()
+  main()
