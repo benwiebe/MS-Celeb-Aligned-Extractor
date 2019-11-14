8cf48fc8291107839183622c1a55b5548d169baf
Ben W <me@t3kbau5.com>
2019-11-14 09:56:54 -0500

    Add cleaned list support [untested]

--- tsv_extractor.py
+++ tsv_extractor.py
@@ -13,28 +13,54 @@ import argparse
 ############### GLOBALS ###############
 inputTSV = ''
 outputFolder = ''
+cleanedList = ''
+
+cleanedDict = dict()
 
 ############### SETUP METHOD ###############
 def setup():
   global inputTSV
   global outputFolder
+  global cleanedList
 
   parser = argparse.ArgumentParser(description='Process MS Celeb TSV file')
   parser.add_argument('inputTSV', help='MS Celeb TSV to extract')
   parser.add_argument('outputFolder', help='Path to extract images to')
+  parser.add_argument('-c', '--cleaned-list', help='Path to file with cleaned classifications', target='cleanedList')
   args = parser.parse_args()
 
   inputTSV = os.path.normpath(args.inputTSV)
   outputFolder = os.path.normpath(args.outputFolder)
+  cleanedList = os.path.normpath(args.cleanedList)
+
+############### METHOD TO LOAD CLEANED DATA ###############
+def loadCleanedData():
+  with open(cleanedList) as file:
+    line = file.readline()
+    while line:
+      parts = line.split(' ')
+      parts[1].replace('/', os.path.sep)
+      cleanedDict[parts[1]] = parts[0]
+
+      line = file.readline()
+
+def genFilename(entity, searchRank, faceId):
+  return entity + os.path.sep + searchRank + "_" + faceId + ".jpg"
 
 ############### MAIN METHOD ###############
 def main():
+
+  if cleanedList != '' and cleanedList != None:
+    print("Loading cleaned data list (this make take a LONG TIME)")
+    loadCleanedData()
+    print("Cleaned data list loaded!")
+
   fid = open(inputTSV, "r")
   if not os.path.exists(outputFolder):
     os.mkdir(outputFolder)
 
   with open(inputTSV, "r", encoding='utf-8') as fid:
-    with open(outputFolder + '/bboxes.txt', 'w') as bbox_file:
+    with open(outputFolder + os.path.sep + 'bboxes.txt', 'w') as bbox_file:
       while True:
         line = fid.readline()
         if line:
@@ -44,12 +70,17 @@ def main():
           # 4: FaceID
           # 5: bbox
           # 6: img_data
-          filename = data_info[0] + os.path.sep + data_info[1] + "_" + data_info[4] + ".jpg"
+          filename = genFilename(data_info[0], data_info[1], data_info[4])
           bbox = struct.unpack('ffff', base64.b64decode(data_info[5]))
           bbox_file.write(filename + " "+ (" ".join(str(bbox_value) for bbox_value  in bbox)) + "\n")
 
           img_data = base64.b64decode(data_info[6])
           output_file_path = outputFolder + os.path.sep + filename 
+          if output_file_path in cleanedDict:
+            orig_path = output_file_path
+            output_file_path = outputFolder + os.path.sep + genFilename(cleanedDict[orig_path], data_info[1], data_info[4])
+            del cleanedDict[orig_path]
+
           if os.path.exists(output_file_path):
             print(output_file_path + " exists")
 
